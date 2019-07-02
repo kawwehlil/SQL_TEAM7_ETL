1b3004c3b15e4f037fc1d4a08a92073603d1c897
kawwehlil <43788283+kawwehlil@users.noreply.github.com>
2019-07-02 14:11:44 -0400

    Update and rename DataDescription.md to DataDescription.pl

---
+++ DataDescription.pl
@@ -0,0 +1,12 @@
+## Database Description
+
+#### Data source
+This dataset is an ensemble of data collected from TMDB and GroupLens. The Movie Details, Credits and Keywords have been collected from the TMDB Open API. 
+
+Please be reminded that the TMDb API used in this project is not endorsed or certified by TMDb. Their API also provides access to data on many additional movies, actors and actresses, crew members, and TV shows. You can try it for yourself here. The Movie Links and Ratings have been obtained from the Official GroupLens website. The files are a part of the dataset available here.
+
+#### Why choose this data 
+Datasets collected by GroupLens are filtered semi-structured dataset which provides rich information in any period that we want. After due diligence the website, we conclude that GroupLens is a valid website and provide credible, reliable, organized, data. To obtain relevant movie data from GroupLens is convenient for us to conduct further analysis and to use for visualization. 
+  
+#### Sample of our dataset 
+For the display,  we split the 22 columns of data into 8 pieces, since each row is too long due to the column number. Since this is only a sample of our total dataset, we only selected one row as an example. 
